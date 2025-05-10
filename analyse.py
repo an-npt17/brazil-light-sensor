f55cee3e09209f747bcb7c8b862f1793d182b7da
an-npt17 <truongan1412@gmail.com>
2025-05-10 17:47:47 +0700

    .

---
+++ analyse.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import plotly.express as px
+import polars as pl
+from matplotlib import pyplot as plt
+
+
+def analyse_data(df: pd.DataFrame) -> None:
+    df["created_at"] = pd.to_datetime(df["created_at"], format="%Y-%m-%d %H:%M:%S %Z")
+    df["created_at"] = df["created_at"].dt.tz_convert("Asia/Ho_Chi_Minh")
+    print(df.head())
+    plt.figure(figsize=(30, 10))
+    plt.plot(
+        df["created_at"],
+        df["field1"],
+    )
+    plt.title("Value over Time")
+    plt.xlabel("Created At")
+    plt.ylabel("Value")
+    plt.savefig("plot.png")
+    fig = px.line(df, x="created_at", y="field1", title="Sensor over Time")
+    fig.update_layout(xaxis_title="Created At", yaxis_title="Value")
+    fig.show()
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("data.csv")
+    analyse_data(df)
---
+++ get_api_key.py
@@ -0,0 +1,39 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_api_key():
+    """
+    Fetch the API key from environment variables or a .env file.
+
+    Returns:
+        str: The API key.
+    """
+    api_key = os.getenv("API_KEY")
+    channel_id = os.getenv("CHANNEL_ID")
+    if api_key is None or channel_id is None:
+        raise ValueError(
+            "API_KEY and CHANNEL_ID must be set in the environment variables."
+        )
+    return api_key, channel_id
+
+
+def get_data():
+    api_key, channel_id = get_api_key()
+    url = f"https://api.thingspeak.com/channels/{channel_id}/feeds.csv"
+    params = {"api_key": api_key, "results": 100000}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        with open("data.csv", "wb") as file:
+            file.write(response.content)
+        print("Data downloaded successfully.")
+    else:
+        print(f"Failed to download data: {response.status_code}")
+
+
+if __name__ == "__main__":
+    get_data()
